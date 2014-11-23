5497b5f0a10d9f90f02564e8e921d7f7861e5458
Stefan Mitic <corcrash@gmail.com>
2014-11-23 04:47:27 +0100

    Da gurnem jos jednom,.

---
+++ Web/routes/class.js
@@ -0,0 +1,42 @@
+/**
+ * Created by MilenCHE on 11/23/2014.
+ */
+var express = require('express');
+var router = express.Router();
+
+var classes = require('../models/class');
+var profile = require('../models/profile');
+
+router.post('/:username', function(req, res) {
+
+    Profile.findOne({teacher: req.params.teacher}, function(err, profile){
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        res.send(profile);
+    })
+});
+
+router.put('/:username', function(req, res){
+    var profile = new Profile();
+
+    profile.first_name = req.body.first_name;
+    profile.last_name = req.body.last_name;
+    profile.picture = req.body.picture;
+    profile.password = req.body.password;
+    profile.tags_interested = req.body.tags_interested;
+    profile.tags_teach = req.body.tags_teach;
+
+    Profile.findOneAndUpdate({username: req.params.username}, profile, {upsert: true}, function (err, egsistingData){
+        if(err){
+            console.log(err);
+            res.send("{success: false }");
+        }
+
+        res.send("{success: true }");
+    });
+});
+
+module.exports = router;
